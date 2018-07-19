d40a2afba23f20039a20f727734cd5e385346a78
ANGELICA GARCES <abgs15@gmail.com>
2018-07-19 10:09:24 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# juego-f
+# proyecto
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Principal</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="/img/icono.png">
+</head>
+<body id="bodyFondo">
+    <div class="container"> 
+        
+        
+  
+  <div id="logo"><img src="img/rompecabeza.png" id="nombreRmp" class="img-fluid"> </div>
+  
+        
+<div id="boton">
+    
+    <div>Usuario:
+    <input id="username" type="usuario" placeholder="Usuario">
+    </div>
+    <br>
+    <div>Clave: 
+        <input id="password" type="password" placeholder="Clave">
+    </div>
+    
+    
+    <br>
+  <div>
+      
+     <button id="validar"> Jugar </button>
+      
+      </div>
+    </div>
+    </div>
+    <div id="audioError">    
+    </div>
+    
+     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
